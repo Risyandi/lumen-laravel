d12ff22d6c41ebe6da9f53b86c24d8aa35e90a16
risyandi <risyandi.94@gmail.com>
2020-02-27 20:51:31 +0700

    add file migration

---
+++ database/migrations/2020_02_27_100544_create_authors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->increments('id_authors');
+            $table->string('name_authors');
+            $table->string('email_authors');
+            $table->string('github_authors');
+            $table->string('twitter_authors');
+            $table->string('location_authors');
+            $table->string('latest_article_published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
