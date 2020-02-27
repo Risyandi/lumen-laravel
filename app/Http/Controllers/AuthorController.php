2e007e8d453af3cc74dcfd0cb95d29975f491c83
risyandi <risyandi.94@gmail.com>
2020-02-27 20:51:03 +0700

    adding controller authors

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Authors;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /** show all authors */
+    public function showAll()
+    {
+        return response()->json(Authors::all());
+    }
+
+    /** show one authors by id */
+    public function showOne($id)
+    {
+        return response()->json(Authors::find($id));
+    }
+
+    /** create new authors */
+    public function createOne(Request $request)
+    {
+        $author = Authors::create($request->all());
+        return response()->json($author, 201);
+    }
+
+    /** delete one authors by id */
+    public function deleteOne($id)
+    {
+        Authors::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+
+    /** update or edit authors by id */
+    public function updateOne($id, Request $request)
+    {
+        $author = Authors::findOrFail($id);
+        $author->update($request->all());
+    }
+}
