c5bc891395f57a7c35a0adc5081b20635f6b1f3f
risyandi <risyandi.94@gmail.com>
2020-02-27 20:49:32 +0700

    modified controller apilumen

--- app/Http/Controllers/apiLumenController.php
+++ app/Http/Controllers/apiLumenController.php
@@ -7,53 +7,48 @@ use Illuminate\Http\Request;
 
 class apiLumenController extends Controller
 {
-    /* function for construct controller
-    * ----------------------------------    
-    public function __construct()
-        {
-    }
-    * ----------------------------------
-    */
-
-    /** function index */
-    public function index(){
+    /** show all apilumen */
+    public function showAll()
+    {
         $data = ModelApiLumen::all();
         return response($data);
     }
-    
-    /** function show data based on id*/
-    public function show($id){
-        $data = ModelApiLumen::where('id',$id)->get();
+
+    /** show one apilumen by id */
+    public function showOne($id)
+    {
+        $data = ModelApiLumen::where('id', $id)->get();
         return response($data);
     }
-    
-    /** function menyimpan data based on input request*/
-    public function store(Request $request){
+
+    /** create new apilumen */
+    public function createOne(Request $request)
+    {
         $data = new ModelApiLumen();
         $data->activity = $request->input('activity');
         $data->description = $request->input('description');
         $data->save();
-        
-        return response('berhasil menambahkan data!');
+
+        return response('create successfully');
     }
-    
-    /** function mengubah data based on input request dan id*/
-    public function update(Request $request, $id){
-        $data = ModelApiLumen::where('id',$id)->first();
+
+    /** update or edit apilumen by id */
+    public function updateOne(Request $request, $id)
+    {
+        $data = ModelApiLumen::where('id', $id)->first();
         $data->activity = $request->input('activity');
         $data->description = $request->input('description');
         $data->save();
-        
-        return response('berhasil merubah data!');
+
+        return response('updated successfully');
     }
-    
-    /** function menghapus data based on id*/
-    public function destroy($id){
-        $data = ModelApiLumen::where('id',$id)->first();
+
+    /** delete one apilumen by id */
+    public function deleteOne($id)
+    {
+        $data = ModelApiLumen::where('id', $id)->first();
         $data->delete();
-        
-        return response('berhasil menghapus data!');
+
+        return response('delete successfully');
     }
-
 }
-
